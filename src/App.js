f3168646f9e55ba70a1cd9d40188da27b0600ec6
Daniel Spencer <daniel@pairanything.com>
2023-02-14 16:35:09 -0800

    alpha branch 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
-import './App.css';
+import React from 'react';
+
+const { useState } = React;
 
 function App() {
+
+  const [gamemode, setGamemode] = useState('start');
+
   return (
-    <div className="App">
-      Poke Battle alpha
+    <div>
+      {gamemode === 'start' && <div>Start</div>}
+      {gamemode === 'battle' && <div>Battle</div>}
+      {gamemode === 'gameOver' && <div>Game Over</div>}
     </div>
   );
 }
 
 export default App;
+
